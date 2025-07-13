6f157044226c2eca2df809a5bd58f5e33aa59461
ramadha <ramadhameepage@gmail.com>
2025-07-13 21:22:01 +1200

    [STYLE] added mobile-first styles for Card & Menu

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import * as styles from '../styles/CardForm.module.scss';
 
 interface CardFormProps {
   onMenuClick: () => void;
 }
 
 const CardForm: React.FC<CardFormProps> = ({ onMenuClick }) => (
-  <div>
+  <div className={styles.container}>
     <Header title="Register Card Form" onMenuClick={onMenuClick} />
-    <form>
+    <form className={styles.form}>
       <label htmlFor="cardNumber">Card Number</label>
       <input id="cardNumber" type="text" placeholder="1234 5678 9012 3456" />
 
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import * as styles from '../styles/Menu.module.scss';
 
 interface MenuProps {
   onBack: () => void;
 }
 
 const Menu: React.FC<MenuProps> = ({ onBack }) => (
-  <div>
+  <div className={styles.container}>
     <Header title="Menu" onBackClick={onBack} />
     <p>This is the menu content.</p>
   </div>
