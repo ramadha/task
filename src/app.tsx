5dd928b67d0542edfdf8db48d929339ad1142ae9
ramadha <ramadhameepage@gmail.com>
2025-07-13 19:29:59 +1200

    [CORE] initialize the project

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1> Hello React + TS + SCSS!</h1>
+    </div>
+  );
+};
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app";
+import "./styles/global.scss";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(<App />);
